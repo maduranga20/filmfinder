032ab76c62460e6e37258dfccfe3470807c4d6b0
maduranga20 <madurangadesign22@gmail.com>
2024-12-19 22:08:43 +0530

    Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ApiAccess from './Component/ApiAccess';
 
 function App() {
   return (
     <div className="App">
-      
+      <ApiAccess/>
     </div>
   );
 }
