7497f767ca4d9c3b5ef5d7c33726d20d1700352e
maduranga20 <madurangadesign22@gmail.com>
2024-12-23 21:47:12 +0530

    impliment list

--- src/Component/ApiAccess.js
+++ src/Component/ApiAccess.js
@@ -1,32 +1,29 @@
-import { useState, useEffect } from 'react';
-// import Movilist from './Movilist';
+// import { useState, useEffect } from 'react';
 
+// const ApiAccess = () => {
+//   const [list, setList] = useState([]);
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((data) => {
+//         // console.log(data);
+//         setList(data);
+//       });
+//   }, []);
+//   return (
 
+//     list
+//   );
+// };
+// export default ApiAccess;
 
-const ApiAccess = () => {
-  const [photos, setList] = useState([]);
-
-  
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        // console.log(data);
-        setList(data);
-      });
-  }, []);
-
-  
-  // console.log(photos);
-  // <Movilist list={photos}/>
+export function Database() {
+  const API_KEY='97803665fe253e44e5d657dd15ecad20';
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=Avengers&page=1&include_adult=false`;
+//  const url = `https://jsonplaceholder.typicode.com/posts`;
   return (
-    <div>
-        <p>ApiAcess</p>
-    
-    </div>
+    url
   );
-};
-export default ApiAccess;
+}
--- src/Component/Movilist.js
+++ src/Component/Movilist.js
@@ -1,38 +1,76 @@
-// import { useState, useEffect } from 'react';
 import React from 'react'
-import ApiAccess from './ApiAccess'
+import { useState, useEffect } from 'react';
+// import ApiAccess from './ApiAccess'
+
+
+import { Database } from './ApiAccess';
 
 
 
-function FilmList(props){
- console.log(props.getList);
- 
-  return (
-    <h6>setupList</h6>
-  )
-     
-}
 
- function Movilist() {
+function Movilist() {
+  const [ list, setList ] = useState([]);
 
-   // {photos.map((photo) => (
+  // {photos.map((photo) => (
   //   <img key={photo.id} src={photo.url} alt={photo.title} width={100} />
   // ))}
 
   // const API_KEY='97803665fe253e44e5d657dd15ecad20';
   // const url=`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=Avengers&page=1&include_adult=false`
- 
-// console.log(props.list);
 
-const check=<ApiAccess/>
+  // console.log(props.list);
+
+  const check = Database();
+
+  // console.log(check);
+
+  useEffect(() => {
+    fetch(check)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setList(data);
+      });
+  }, [ check ]);
+
+  // console.log(list);
+  //  const arrangeValues =list.map((track) => ({
+  //     id: track.id,
+  //     name: track.title,
+
+  //   }));
+
+// const {select}=list.page;
+  // const arrangeValues =list.results.map((track) => ({
+  //       id: track.id,
+  //       name: track.title,
+  //     }));
+
+
+  //     console.log(arrangeValues)
+  // const postList = list.results.map(function (post) {
+  //   return (
+  //     <div key={post.id}>
+  //       <p> title: {post.original_title}</p>
+  //     </div>
+  //   )
+  // })
+
   
+  console.log(list.map((track)=>track.id));
+
+
+
   return (
-   
+
     <>
-     <div>Movilist</div>
-     <FilmList getList={check}/>
+      <div>Movilist</div>
+     
+
     </>
-    
+
   )
 }
 
